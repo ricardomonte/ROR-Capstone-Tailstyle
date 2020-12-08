8e4cbf8fc15850c9c849de9072934ae602aa018f
ricardo <ramn21@gmail.com>
2020-12-08 18:36:34 +0100

    add header and footer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,8 +4,18 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(params.require(:user).permit(:name))
-    byebug
-    @user.save
+    @user = User.new(users_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirec_to root
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def users_params
+    params.require(:user).permit(:username, :name, :email, :password)
   end
 end
