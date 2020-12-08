2ee4c43c7cf9a43248b502c8ca9ab2cff5952919
ricardo <ramn21@gmail.com>
2020-12-09 00:13:44 +0100

    add routs for login, logout and articles

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "articles#index"
   resources :users
   get 'login', to: 'sessions#new' 
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
+  resources :articles
 end
