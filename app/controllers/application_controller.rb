6e4d9b9da5a9080ef734f55be3a12ab71e2fd3e1
ricardo <ramn21@gmail.com>
2020-12-08 02:46:39 +0100

    add current user and signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user, :signed_in?
+
+  def current_user
+    @current_user ||= session[:user_id] &&
+                      User.find_by(id: session[:user_id])
+  end
+
+  # rubocop:disable Style/DoubleNegation
+  def signed_in?
+    !!current_user
+  end
+  # rubocop:enable Style/DoubleNegation
+
 end
