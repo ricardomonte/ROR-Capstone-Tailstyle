347bc2314c0564275e2c1c248ea596c1d0cef7cb
ricardo <ramn21@gmail.com>
2020-12-09 18:30:17 +0100

    add create action in article controller

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,21 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.includes(:user).all
   end
+
+  def new
+    @article = Article.new
+    @categories = Category.all
+  end
+
+  def create
+    @article = current_user.articles.build(title: params[:article][:title], text: params[:article][:text], image: params[:article][:image])
+    category = params[:article][:categories].to_i
+    if @article.save
+      @article.category_ids = category
+      flash[:notice] = 'Article has been created.'
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 end
