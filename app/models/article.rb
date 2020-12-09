5daa3c7bec9d7f2470e76453cde9c210ccd2e4f7
ricardo <ramn21@gmail.com>
2020-12-09 16:02:47 +0100

    add shrine gem, config shrine

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
+
+  include ImageUploader::Attachment(:image)
+  
   belongs_to :user, foreign_key: :author_id, class_name: 'User'
 
   has_and_belongs_to_many :categories
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_08_224402) do
+ActiveRecord::Schema.define(version: 2020_12_09_144043) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 2020_12_08_224402) do
     t.bigint "author_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.text "image_data"
     t.index ["author_id"], name: "index_articles_on_author_id"
   end
 
