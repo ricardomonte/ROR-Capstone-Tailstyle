a21d0ee01dc5f63f3444b9c7965eb8cb2ff2b1ce
ricardo <ramn21@gmail.com>
2020-12-09 22:38:46 +0100

    add fash create, add destroy action in session

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,17 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(username: params[:session][:username])
-    session[:user_id] = user.id
+    if user
+      session[:user_id] = user.id
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Wrong username"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
     redirect_to root_path
   end
 end
