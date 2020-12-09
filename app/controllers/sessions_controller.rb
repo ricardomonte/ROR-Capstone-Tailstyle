70152a7b42b17f2434f556f91d16b0c091db8072
ricardo <ramn21@gmail.com>
2020-12-09 18:30:51 +0100

    fix create action in session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(name: params[:name])
+    user = User.find_by(username: params[:session][:username])
     session[:user_id] = user.id
-
+    redirect_to root_path
   end
 end
