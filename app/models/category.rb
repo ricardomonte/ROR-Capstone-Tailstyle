617ff1f001d51747272a8077bf5e812abbbc78e4
ricardo <ramn21@gmail.com>
2020-12-09 22:37:52 +0100

    add scope in article and category models

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,7 @@ class Category < ApplicationRecord
   has_and_belongs_to_many :articles
 
   scope :order_by_priority, -> { order(priority: :desc) }
+  
+  scope :order_by_creat, -> { order(created_at: :asc) }
+
 end
