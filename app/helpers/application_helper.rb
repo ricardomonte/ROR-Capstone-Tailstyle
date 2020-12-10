a37340602915434698f587203696364adfdd2eb4
ricardo <ramn21@gmail.com>
2020-12-10 18:54:08 +0100

    add navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+
+  def nav_list
+    category = Category.all
+    category.each do | cat |
+      concat content_tag(:li, (link_to cat.name, category_path(cat.id), class: 'nav-link'),  class: 'nav-item') 
+    end
+  end
 end
