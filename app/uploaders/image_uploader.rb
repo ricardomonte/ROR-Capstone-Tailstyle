93b5820375dddd220bd203ee39b23da9aab9177c
ricardo <ramn21@gmail.com>
2020-12-12 03:44:04 +0100

    add presing end point

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -1,3 +1,13 @@
 class ImageUploader < Shrine
-  # plugins and uploading logic 
+  plugin :presign_endpoint, presign_options: -> (request) {
+  # Uppy will send the "filename" and "type" query parameters
+    filename = request.params["filename"]
+    type     = request.params["type"]
+
+    {
+      content_disposition:    ContentDisposition.inline(filename), # set download filename
+      content_type:           type,                                # set content type (defaults to "application/octet-stream")
+      content_length_range:   0..(10*1024*1024),                   # limit upload size to 10 MB
+    }
+  }
 end
--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -1,25 +1,51 @@
-require "shrine"
-require "shrine/storage/file_system"
-require "shrine/storage/s3"
+require 'shrine'
 
-s3_options = Rails.application.credentials.s3
-
-Shrine.storages = {
-  cache: Shrine::Storage::S3.new(prefix: "cache", **s3_options),
-  store: Shrine::Storage::S3.new(**s3_options),
-}
+if Rails.env.development?
+  require "shrine/storage/file_system"
+  Shrine.storages = {
+    cache: Shrine::Storage::FileSystem.new("public", prefix: "uploads/cache"),
+    store: Shrine::Storage::FileSystem.new("public", prefix: "uploads")
+  }
+elsif Rails.env.test?
+  require 'shrine/storage/memory'
+  Shrine.storages = {
+    cache: Shrine::Storage::Memory.new,
+    store: Shrine::Storage::Memory.new
+  }
+else
+  require "shrine/storage/s3"
+  s3_options = {
+    access_key_id:     Rails.application.credentials.s3.access_key_id,
+    secret_access_key: Rails.application.credentials.s3.secret_access_key,
+    region:            Rails.application.credentials.s3.region,
+    bucket:            Rails.application.credentials.s3.bucket
+  }
+  Shrine.storages = {
+    cache: Shrine::Storage::S3.new(prefix: "cache", **s3_options),
+    store: Shrine::Storage::S3.new(prefix: "store", **s3_options)
+  }
+end
 
 Shrine.plugin :activerecord           # loads Active Record integration
 Shrine.plugin :cached_attachment_data # enables retaining cached file across form redisplays
 Shrine.plugin :restore_cached_data    # extracts metadata for assigned cached files
-Shrine.plugin :presign_endpoint, presign_options: -> (request) {
-  # Uppy will send the "filename" and "type" query parameters
-  filename = request.params["filename"]
-  type     = request.params["type"]
+Shrine.plugin :presign_endpoint
 
-  {
-    content_disposition:    ContentDisposition.inline(filename), # set download filename
-    content_type:           type,                                # set content type (defaults to "application/octet-stream")
-    content_length_range:   0..(10*1024*1024),                   # limit upload size to 10 MB
-  }
-}
+
+
+# require "shrine"
+# require "shrine/storage/file_system"
+# require "shrine/storage/s3"
+
+# s3_options = Rails.application.credentials.s3
+
+# Shrine.storages = {
+#   cache: Shrine::Storage::S3.new(prefix: "cache", **s3_options),
+#   store: Shrine::Storage::S3.new(**s3_options),
+# }
+
+# Shrine.plugin :activerecord           # loads Active Record integration
+# Shrine.plugin :cached_attachment_data # enables retaining cached file across form redisplays
+# Shrine.plugin :restore_cached_data    # extracts metadata for assigned cached files
+# Shrine.plugin :presign_endpoint
+
