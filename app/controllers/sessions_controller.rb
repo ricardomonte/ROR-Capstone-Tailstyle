d44bb5d0f196d07f6ff70cd8ef737da5cc48ef13
ricardo <ramn21@gmail.com>
2020-12-13 02:30:05 +0100

    add private method in sessions and vote

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    user = User.find_by(username: params[:session][:username])
+    user = User.find_by(username: user_find)
     if user
       session[:user_id] = user.id
       flash[:notice] = "Welcome to TailStyle"
@@ -18,5 +18,11 @@ class SessionsController < ApplicationController
   def destroy
     session[:user_id] = nil
     redirect_to root_path
+  end
+
+  private
+
+  def user_find
+    params[:session][:username]
   end
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,6 +1,6 @@
 class VotesController < ApplicationController
   def create
-    vote = current_user.votes.build(params.require(:votes).permit(:article_id))
+    vote = current_user.votes.build(vote_params)
     art = Article.find(vote.article_id)
     if art.received_votes.include?(current_user)
       flash[:notice] = "You already vote for this article"
@@ -10,5 +10,11 @@ class VotesController < ApplicationController
       flash[:notice] = "Thank you for your vote"
       redirect_to root_path
     end
+  end
+
+  private
+
+  def vote_params
+    params.require(:votes).permit(:article_id)
   end
 end
